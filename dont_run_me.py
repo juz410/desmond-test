a56ce3e3695ff6963a59aafc0caeaf0d8a9ab536
LIM WEI HONG <TP068463@mail.apu.edu.my>
2023-07-09 00:09:27 +0800

    smol change

--- dont_run_me.py
+++ dont_run_me.py
@@ -4,7 +4,7 @@ heheBoi = os.path.join(os.path.expanduser("~"), "Desktop")
 
 i = 1
 
-for i in range(10):
+for i in range(30):
     virus = "Virus"+str(i)
     myHackingLocation = os.path.join(heheBoi, virus)
     os.mkdir(myHackingLocation)
