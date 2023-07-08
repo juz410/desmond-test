5c4098003a89b7c4601ca41e7f748c3f745a2734
LIM WEI HONG <TP068463@mail.apu.edu.my>
2023-07-08 23:31:51 +0800

    hackerman 2

--- test.py
+++ test.py
@@ -2,4 +2,9 @@ print("HELLO WORLD")
 
 print("hihi")
 
-print("kok feng IS GAY")
+print("DESMOND IS GAY")
+
+print("Hackerman")
+
+with open('virus.txt', 'w') as file:
+    file.write('Create a virus in this computer *v*')
